b5c0cec1fd0984fd58f4e0af78dc0b4643350686
Rei Koshka <purrogurama@gmail.com>
2015-11-12 17:54:07 +0500

    Feature of calling action once per loop (for .NET 4.5 only). Awaitable HTTP-requests. Build v2.9.2 for .NET Framework 4.0 (full) uploaded. Build v2.9.2 for .NET Framework 4.5 uploaded.

---
+++ Src/Bytloos/Invoker.cs
@@ -0,0 +1,41 @@
+﻿#if NET45
+using System;
+using System.Runtime.CompilerServices;
+using Bytloos.Extensions;
+#endif
+
+namespace Bytloos
+{
+    /// <summary>
+    /// Tools for calls.
+    /// </summary>
+    public static class Invoker
+    {
+        #if NET45
+
+        private static string previousCallID;
+
+        /// <summary>
+        /// Calls action once per loop.
+        /// </summary>
+        /// <param name="action">Target action.</param>
+        /// <param name="sourceFilePath">Passes CallerFilePathAttribute value.</param>
+        /// <param name="memberName">Passes CallerMemberNameAttribute value.</param>
+        /// <param name="sourceLineNumber">Passes CallerLineNumberAttribute value.</param>
+        public static void CallOnce(
+                                Action  action,
+            [CallerFilePath]    string  sourceFilePath      = "",
+            [CallerMemberName]  string  memberName          = "",
+            [CallerLineNumber]  int     sourceLineNumber    = 0)
+        {
+            var callerInfoHash = (sourceFilePath + memberName + sourceLineNumber).MD5();
+
+            if (callerInfoHash != previousCallID)
+                action();
+
+            previousCallID = callerInfoHash;
+        }
+
+        #endif
+    }
+}
