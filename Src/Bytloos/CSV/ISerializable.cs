0fbb8098e3062c7cc0c41ff6735f5f8f265dce4a
Rei Koshka <purrogurama@gmail.com>
2015-12-12 20:31:36 +0500

    CSV serialization controlled by interfaces Build v2.9.9 for .NET Framework 4.0 (full) uploaded Build v2.9.9 for .NET Framework 4.5 uploaded

---
+++ Src/Bytloos/CSV/ISerializable.cs
@@ -0,0 +1,21 @@
+﻿namespace Bytloos.CSV
+{
+    /// <summary>
+    /// Provides CSV document serialization for fields and properties
+    /// of objects of classes implementing this interface.
+    /// </summary>
+    public interface ISerializable
+    {
+        /// <summary>
+        /// Gets string representation of field.
+        /// </summary>
+        /// <returns>String representation of field.</returns>
+        string GetStringValue();
+
+        /// <summary>
+        /// Parses object from it's string representation.
+        /// </summary>
+        /// <param name="value">String representation of field.</param>
+        void SetValueFromString(string value);
+    }
+}
