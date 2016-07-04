5cabd5aba186718787806eb1f765ce96166da82d
Rei Koshka <purrogurama@gmail.com>
2016-07-04 12:30:51 +0500

    Getting and setting private field hack extension

--- Src/Bytloos/Properties/AssemblyInfo.cs
+++ Src/Bytloos/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle            ("Bytloos")]
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture          ("")]
 [assembly: ComVisible               (false)]
 [assembly: Guid                     ("8c1c434d-b9f6-443a-9ea6-e276528ff7aa")]
-[assembly: AssemblyVersion          ("2.9.9.5")]
-[assembly: AssemblyFileVersion      ("2.9.9.5")]
+[assembly: AssemblyVersion          ("2.9.9.6")]
+[assembly: AssemblyFileVersion      ("2.9.9.6")]
