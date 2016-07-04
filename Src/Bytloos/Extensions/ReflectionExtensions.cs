c990183ba5fb8df303ac79ba9190cf0fe8f2dcb7
Rei Koshka <purrogurama@gmail.com>
2016-07-04 12:30:51 +0500

    Getting and setting private field hack extension.

---
+++ Src/Bytloos/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace Bytloos.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static TValue GetFieldValue<TObject, TValue>(this TObject source, string fieldName)
+        {
+            var fieldInfo
+                = typeof(TObject).GetField(
+                    name:           fieldName,
+                    bindingAttr:    BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var result = (TValue)fieldInfo.GetValue(source);
+
+            return result;
+        }
+
+        public static void SetFieldValue<TObject, TValue>(this TObject source, string fieldName, TValue value)
+        {
+            var fieldInfo
+                = typeof(TObject).GetField(
+                    name:           fieldName,
+                    bindingAttr:    BindingFlags.Instance | BindingFlags.NonPublic);
+
+            fieldInfo.SetValue(source, value);
+        }
+    }
+}
