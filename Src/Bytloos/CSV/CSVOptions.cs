5f79a2d7fbb263b03d53256b4002e3f63c400301
Rei Koshka <purrogurama@gmail.com>
2018-03-10 17:49:46 +0500

    Save reading CSV from string and file (with tests)

---
+++ Src/Bytloos/CSV/CSVOptions.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Bytloos.CSV
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CSVOptions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CSVOptions Default
+        {
+            get
+            {
+                return new CSVOptions
+                {
+                    SwapQuotes = false,
+                    Delimiter = Cell.DEFAULT_DELIMITER,
+                    QuoteChar = Cell.DEFAULT_QUOTE,
+                    Encoding = Encoding.UTF8
+                };
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool SwapQuotes { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public char Delimiter { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public char QuoteChar { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Encoding Encoding { get; set; }
+    }
+}
