8efdd39f71927b41510d54f97afa8c397f869c55
Rei Koshka <purrogurama@gmail.com>
2018-03-10 12:26:00 +0500

    Save CSV refactoring

---
+++ Src/Bytloos/CSV/Row.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Bytloos.CSV
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Rows.
+    /// </summary>
+    public class Row : List<Cell> { }
+}
