e099b33f41fde82f4868f8cc070e91cc73e8806d
Rei Koshka <purrogurama@gmail.com>
2018-03-18 16:01:58 +0500

    Version updated to 3.1.0.

--- Src/Bytloos/Properties/AssemblyInfo.cs
+++ Src/Bytloos/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture          ("")]
 [assembly: ComVisible               (false)]
 [assembly: Guid                     ("8c1c434d-b9f6-443a-9ea6-e276528ff7aa")]
-[assembly: AssemblyVersion          ("3.0.1.0")]
-[assembly: AssemblyFileVersion      ("3.0.1.0")]
+[assembly: AssemblyVersion          ("3.1.0.0")]
+[assembly: AssemblyFileVersion      ("3.1.0.0")]
